6b2e14db81e90ea5c7239c41cd98a656438575d9
Bob <bkane90@gmail.com>
2021-03-21 11:33:48 -0400

    fix post request

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/api', (req, res, next) => {
     res.send('API Status: Running')
 });
 
-app.post('/api/email', (req, res, next) => {
+app.post('https://robertkanejr.netlify.app/email', (req, res, next) => {
     sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
         to: 'bkane90@gmail.com',
