6c59b6fd4bf5f224a6c1c739e04e925e23d0f74e
Bob <bkane90@gmail.com>
2021-07-10 17:05:45 -0400

    test with OAuth2

--- app.js
+++ app.js
@@ -1,38 +1,71 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const nodemailer = require("nodemailer");
-const cors = require("cors");
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const nodemailer = require("nodemailer");
+// const cors = require("cors");
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-const smtpTrans = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS,
-    },
-});
+// const smtpTrans = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         user: process.env.GMAIL_USER,
+//         pass: process.env.GMAIL_PASS,
+//     },
+// });
 
-app.post("/contact", (req, res) => {
-    const mailOpts = {
-        to: process.env.GMAIL_USER,
-        subject: "Portfolio Site Contact Form Submission",
-        html: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
-    };
+// app.post("/contact", (req, res) => {
+//     const mailOpts = {
+//         to: process.env.GMAIL_USER,
+//         subject: "Portfolio Site Contact Form Submission",
+//         html: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
+//     };
 
-    smtpTrans.sendMail(mailOpts, (err, info) => {
-        if (err) {
-            res.status(404).send(err);
-        } else {
-            res.send("Message sent succesfully");
-        }
-    });
-});
+//     smtpTrans.sendMail(mailOpts, (err, info) => {
+//         if (err) {
+//             res.status(404).send(err);
+//         } else {
+//             res.send("Message sent succesfully");
+//         }
+//     });
+// });
 
-app.listen(process.env.PORT, () =>
-    console.log(`Server running on port: ${process.env.PORT}.`)
-);
+// app.listen(process.env.PORT, () =>
+//     console.log(`Server running on port: ${process.env.PORT}.`)
+// );
+
+
+
+// function sendEmail(name, email, message) {
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             Accept: "application/json",
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: name,
+//             email: email,
+//             message: message
+//         })
+//     }
+
+//     return fetch("/contact", options)
+//         .then(res => {
+//             if (res.status === 200) {
+//                 Swal.fire({
+//                     icon: 'success',
+//                     title: 'Message sent successfully!',
+//                 })
+//                 form.reset()
+//             } else {
+//                 Swal.fire({
+//                     icon: 'error',
+//                     title: 'Error, please try again!',
+//                 })
+//             }
+//         })
+// }
---
+++ server.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+const express = require('express')
+const cors = require("cors")
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
+
+const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+)
+
+oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+})
+
+const accessToken = oauth2Client.getAccessToken()
+
+const app = express()
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+app.route("/").get(function (req, res) {
+    res.sendFile(process.cwd() + "/public/contactUs.html");
+});
+
+app.post('/contact', (req, response) => {
+    const output = `
+  <p>You have a new contact request</p>
+  <img class="email" src="cid:email" alt="email-image">
+  <h3>Contact details</h3>
+  <ul>
+  <li>FirstName: ${req.body.name}</li>
+  <li>Email: ${req.body.email}</li>
+  <li>Message: ${req.body.message}</li>
+  </ul>`
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: "OAuth2",
+            user: process.env.GMAIL_USER,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: accessToken
+        }
+    })
+
+    const mailOpts = {
+        from: process.env.GMAIL_USER,
+        to: process.env.RECIPIENT,
+        subject: 'New message from Nodemailer-contact-form',
+        html: output
+    }
+
+    smtpTrans.sendMail(mailOpts, (error, res) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Message sent: " + res.message);
+            response.status(200).send(200)
+        }
+    })
+})
+
+const port = process.env.PORT || 5000
+const server = app.listen(port, listening)
+function listening() {
+    console.log(`server running on ${port}`)
+}
