0f6fd4623c5ea6b5e3b215ebf47df066f69e5bce
Bob <bkane90@gmail.com>
2021-07-16 12:55:30 -0400

    add readme

--- server.js
+++ server.js
@@ -24,10 +24,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
-// app.route("/").get(function (req, res) {
-//     res.sendFile(process.cwd() + "/public/contactUs.html");
-// });
-
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
